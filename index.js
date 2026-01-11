37afe288957f38c4889777c96b170bd79fc6a21a
Safuvan K <safuvanvkm@gmail.com>
2026-01-11 23:14:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = 3000;
 
-const DB_URL = "mongodb://localhost:27017/user-db";
+const DB_URL = "mongodb://localhost:27017/userdb";
 
 // middleware passing
 
@@ -17,21 +17,35 @@ mongoose
   .then(() => console.log("Db is connected"))
   .catch((err) => console.log("error found", err));
 
-
 //Define schema
 const useSchema = new mongoose.Schema({
   name: { type: "string", required: true },
   age: { type: Number, required: true },
   email: { type: "string", required: true },
+  phone:{type:Number,required:false},
+  place:{type:String,required:true}
 });
 
 // Model creation
-const User = mongoose.model('user',useSchema,'userdetails')
+const User = mongoose.model("user", useSchema, "userdetails");
 
 //CRUD
 
 //CREATE
-app.post('/users',)
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const insertData = await user.save();
+    res.status(200).json({
+      message: "User created",
+      insertedData: insertData,
+    });
+  } catch {
+    res.status(404).json({
+      message: "User not created",
+    });
+  }
+});
 // create route
 
 app.get("/", (req, res) => {
